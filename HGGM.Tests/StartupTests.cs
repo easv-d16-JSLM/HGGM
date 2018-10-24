499d48def15cf76b4bf913866f24eb3fd9693b6c
J2ghz <j2.00ghz@gmail.com>
2018-10-24 10:10:41 +0200

    Add an integration test

---
+++ HGGM.Tests/StartupTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace HGGM.Tests
+{
+    public class StartupTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public StartupTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task GetReturnsSuccess(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
