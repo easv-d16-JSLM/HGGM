93903662c4a69264a6014590fd1f73f7d691e6e5
J2ghz <j2.00ghz@gmail.com>
2018-11-05 11:34:42 +0100

    add initializer

---
+++ HGGM/Services/LiteDbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HGGM.Models.Identity;
+using LiteDB;
+using Microsoft.AspNetCore.Identity;
+using Serilog;
+
+namespace HGGM.Services
+{
+    public class LiteDbInitializer
+    {
+        private readonly ILogger log = Log.ForContext<LiteDbInitializer>();
+
+        public LiteDbInitializer(LiteRepository repository, UserManager<User> userManager,
+            RoleManager<Role> roleManager)
+        {
+            Initialize(repository, userManager, roleManager).RunSynchronously();
+        }
+
+        private async Task CreateUsers(UserManager<User> userManager)
+        {
+            var results = new List<IdentityResult>
+            {
+                await userManager.CreateAsync(new User {Email = "root@localhost", UserName = "root"}, "root")
+            };
+            results.ForEach(r =>
+            {
+                if (r.Succeeded) return;
+                foreach (var error in r.Errors)
+                    log.Error("User Creation failed with code {code}: {message}", error.Code, error.Description);
+            });
+        }
+
+        private async Task Initialize(LiteRepository repository, UserManager<User> userManager,
+            RoleManager<Role> roleManager)
+        {
+            await CreateUsers(userManager);
+        }
+    }
+}
