bb761e9f6e96afb50a22d14023f525cff56f4bb9
Lasse Nielsen <lpnielsen@hotmail.com>
2018-11-08 10:53:40 +0100

    UserWithRoles ViewModel

---
+++ HGGM/ViewModels/UserWithRolesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HGGM.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HGGM.ViewModels
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public IList<Role> Roles { get; set; }
+    }
+}
