2fdf494d0c5bf7fed9a7d6db63f4ebd41de25e6c
J2ghz <j2.00ghz@gmail.com>
2018-11-12 15:01:34 +0100

    add basic

---
+++ HGGM/Models/Events/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HGGM.Models.Events
+{
+    public class Event
+    {
+    }
+}
---
+++ HGGM/Models/Events/Slot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using HGGM.Models.Identity;
+
+namespace HGGM.Models.Events
+{
+    public class Slot
+    {
+        public string Name { get; set; }
+        public IList<IRequirement> Requirements { get; set; }
+        public IList<SlotSignUp> SignUps { get; set; }
+        public IList<Slot> SubSlots { get; set; }
+    }
+
+    public class SlotSignUp
+    {
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Deleted { get; set; }
+        public Slot Slot { get; set; }
+        public User User { get; set; }
+    }
+
+    public interface IRequirement
+    {
+        //Implement in different classes
+    }
+}
