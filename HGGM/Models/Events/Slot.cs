328dcacb44103b817e1ca0efeb6d8d98014b61ae
J2ghz <j2.00ghz@gmail.com>
2018-11-13 14:22:24 +0100

    Add rest

--- HGGM/Models/Events/Slot.cs
+++ HGGM/Models/Events/Slot.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using HGGM.Models.Identity;
+using LiteDB;
 
 namespace HGGM.Models.Events
 {
+    public class Event
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public User Author { get; set; }
+        public User Publisher { get; set; }
+        [BsonRef(nameof(Slot))] public IList<Slot> Roster { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset Published { get; set; }
+        public DateTimeOffset TakesPlace { get; set; }
+    }
+
+    public enum ApprovalType
+    {
+        Nobody,
+        Children,
+        Descendants,
+        Everybody
+    }
+
     public class Slot
     {
+        public ApprovalType CanApprove { get; set; }
+        public ObjectId Id { get; set; }
         public string Name { get; set; }
         public IList<IRequirement> Requirements { get; set; }
         public IList<SlotSignUp> SignUps { get; set; }
@@ -16,6 +39,7 @@ namespace HGGM.Models.Events
     {
         public DateTimeOffset Created { get; set; }
         public DateTimeOffset? Deleted { get; set; }
+        public ObjectId Id { get; set; }
         public Slot Slot { get; set; }
         public User User { get; set; }
     }
