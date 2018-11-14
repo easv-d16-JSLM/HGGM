e68fe872acce92c53f04e66a49149e36c44afb1a
J2ghz <j2.00ghz@gmail.com>
2018-11-14 13:28:36 +0100

    add taghelper

--- HGGM/Controllers/HomeController.cs
+++ HGGM/Controllers/HomeController.cs
@@ -7,13 +7,6 @@ namespace HGGM.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly MarkdownService _markdown;
-
-        public HomeController(MarkdownService markdown)
-        {
-            _markdown = markdown;
-        }
-
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -36,7 +29,6 @@ namespace HGGM.Controllers
 
         public IActionResult Index()
         {
-            ViewData["Message"] = _markdown.ToHtml("- [x] Markdown works!");
             return View();
         }
 
---
+++ HGGM/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HGGM.Services;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace HGGM.TagHelpers
+{
+    [HtmlTargetElement("markdown",TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class MarkdownTagHelper : TagHelper
+    {
+        private readonly MarkdownService m;
+
+        public MarkdownTagHelper(MarkdownService m)
+        {
+            this.m = m;
+        }
+
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.TagName = "div";
+            output.Attributes.Add("class", "content");
+            var content = output.Content.IsModified ? output.Content.GetContent() :
+                (await output.GetChildContentAsync()).GetContent();
+            output.Content.SetHtmlContent(m.ToHtml(content));
+        }
+    }
+}
