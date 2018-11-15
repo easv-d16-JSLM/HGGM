29fd35c79c2e63ceffd4584dbb5fa9ab1aa0fd9d
J2ghz <j2.00ghz@gmail.com>
2018-11-15 11:36:44 +0100

    remove empty lines

--- HGGM/TagHelpers/MarkdownTagHelper.cs
+++ HGGM/TagHelpers/MarkdownTagHelper.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using HGGM.Services;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace HGGM.TagHelpers
 {
-    [HtmlTargetElement("markdown",TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement("markdown", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class MarkdownTagHelper : TagHelper
     {
         private readonly MarkdownService m;
@@ -17,15 +14,14 @@ namespace HGGM.TagHelpers
             this.m = m;
         }
 
-
-
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagMode = TagMode.StartTagAndEndTag;
             output.TagName = "div";
             output.Attributes.Add("class", "content");
-            var content = output.Content.IsModified ? output.Content.GetContent() :
-                (await output.GetChildContentAsync()).GetContent();
+            var content = output.Content.IsModified
+                ? output.Content.GetContent()
+                : (await output.GetChildContentAsync()).GetContent();
             output.Content.SetHtmlContent(m.ToHtml(content));
         }
     }
