e8829f57152e15cd63d8d0188027cbd5d32aa43d
Morten Andersen <frund123@gmail.com>
2018-11-15 14:02:44 +0100

    Start on tags

---
+++ HGGM/Models/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HGGM.Models.Identity;
+
+namespace HGGM.Models
+{
+    public class Tag
+    {
+        public string TagId { get; set; }
+        public List<User> Users { get; set; }
+        public string TagName { get; set; }
+
+        public Tag(string tagId, List<User> users, string tagName)
+        {
+            TagId = tagId;
+            Users = users;
+            TagName = tagName;
+        }     
+    }
+}
