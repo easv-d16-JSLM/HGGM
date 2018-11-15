f614d068b051f680e19bff5f38303580d3dfe2ce
Morten Andersen <frund123@gmail.com>
2018-11-15 13:30:45 +0100

    Started creating test for rolescontroller

---
+++ HGGM.IntegrationTests/Areas/Roles/RolesControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using AngleSharp.Dom.Html;
+using FluentAssertions;
+using HGGM.IntegrationTests.Helpers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HGGM.IntegrationTests.Areas.Roles
+{
+    public class RolesControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public RolesControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task EmptyRoleDoesNotRegister()
+        {
+            var client = _factory.CreateClient();
+
+            // Arrange
+            var defaultPage = await client.GetAsync("/roles/Create");
+            var content = await HtmlHelpers.GetDocumentAsync(defaultPage);
+
+            //Act
+            var response = await client.SendAsync(
+                (IHtmlFormElement)content.QuerySelector("form[action=\"/roles/Create\"]"),
+                (IHtmlButtonElement)content.QuerySelector("button"));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, defaultPage.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+
+    }
+}
