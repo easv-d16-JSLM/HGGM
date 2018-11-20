5a17b7963c05580ef71d2206e47fb0e8849a4b65
Lasse Nielsen <lpnielsen@hotmail.com>
2018-11-20 10:48:56 +0100

    Successfully store email & personaldata in user model

--- HGGM/Models/Identity/User.cs
+++ HGGM/Models/Identity/User.cs
@@ -1,19 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AspNetCore.Identity.LiteDB.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace HGGM.Models.Identity
 {
-    public class User : AspNetCore.Identity.LiteDB.Models.ApplicationUser
+    public class User : ApplicationUser
     {
-        public DateTime DateOfBirth { get; set; }
-        public string Steam64ID { get; set; }
-        public string TeamspeakUID { get; set; }
-        public string Name { get; set; }
-        public string Headline { get; set; }
-        public string Biography { get; set; }
-        public string Country { get; set; }
         public Guid Avatar { get; set; }
+        public string Biography { get; set; }
+
+        [PersonalData] public string Country { get; set; }
+
+        [PersonalData] public DateTime DateOfBirth { get; set; }
+
+        public string Headline { get; set; }
+
+        [PersonalData] public string Name { get; set; }
+
+        [PersonalData] public string Steam64ID { get; set; }
+
+        [PersonalData] public string TeamspeakUID { get; set; }
     }
 }
