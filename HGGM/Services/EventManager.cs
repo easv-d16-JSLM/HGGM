4c890af8f0d8ad2204e4c16c493110382dcbf860
Morten Andersen <frund123@gmail.com>
2018-11-29 09:45:01 +0100

    New method in EventManager + Small change in notificationService

--- HGGM/Services/EventManager.cs
+++ HGGM/Services/EventManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HGGM.Models;
 using HGGM.Models.Events;
 using HGGM.Models.Identity;
 using HGGM.Services.Authorization.Simple;
@@ -17,16 +18,27 @@ namespace HGGM.Services
     public class EventManager
     {
         private readonly LiteRepository _db;
-        public EventManager(LiteRepository db)
+        private readonly INotificationService _nService;
+        
+        public EventManager(LiteRepository db, INotificationService nService)
         {
+
             _db = db;
+            _nService = nService;
+            
         }
 
         public void AddEvent(Event eEvent)
         {
             eEvent.Created = DateTimeOffset.Now;
             _db.Insert(eEvent);
-            // Insert notification service
+            _nService.NotifyUsers(new Notification()
+            {
+                Message = "Event with name: " + eEvent.Name + " created! Author: " + eEvent.Author + "Takes place: " +
+                          eEvent.TakesPlace,
+                Subject = "Event" + eEvent.Name + " awaiting approval"
+            }, new List<User>(_db.Fetch<User>()));
+
         }
 
         public void EditEvent(Event eEvent)
@@ -73,8 +85,7 @@ namespace HGGM.Services
                 .AuthorizeAsync(context.User, null, new TagRequirement()).GetAwaiter()
                 .GetResult();
 
-            return result.Succeeded;
-            
+            return result.Succeeded;            
         }
 
     }
