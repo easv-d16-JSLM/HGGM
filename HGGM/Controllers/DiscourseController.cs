034e0cf721d723eaaf9a17fd4a42ffc7de6545b5
J2ghz <j2.00ghz@gmail.com>
2018-12-03 13:20:27 +0100

    add logging

--- HGGM/Controllers/DiscourseController.cs
+++ HGGM/Controllers/DiscourseController.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Flurl;
 using HGGM.Models.Identity;
 using HGGM.Services.Discourse;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace HGGM.Controllers
 {
@@ -15,6 +15,7 @@ namespace HGGM.Controllers
     public class DiscourseController : ControllerBase
     {
         private readonly DiscourseService _discourseService;
+        private readonly ILogger _log = Log.ForContext<DiscourseController>();
         private readonly UserManager<User> _userManager;
 
         public DiscourseController(DiscourseService discourseService, UserManager<User> userManager)
@@ -28,15 +29,14 @@ namespace HGGM.Controllers
         {
             var (nonce, returnUrl) = _discourseService.OpenPayload(sso, sig);
             var user = await _userManager.GetUserAsync(User);
-
+            _log.Information("Login request from {user}", user.UserName, user.Id, nonce, returnUrl);
             var (payload, signature) = _discourseService.CreatePayload(nonce, user.Email.Address, user.Id,
                 user.UserName, user.Name,
                 Url.Action("Avatar", "Files", new {id = user.Id}), user.Biography,
                 emailRequireActivation: user.Email.IsConfirmed);
-
             if (returnUrl == null) returnUrl = Request.Headers["Referer"];
-
-            return Redirect(returnUrl.SetQueryParam("sso", payload).SetQueryParam("sig", signature));
+            var url = returnUrl.SetQueryParam("sso", payload).SetQueryParam("sig", signature);
+            return Redirect(url);
         }
     }
 }
