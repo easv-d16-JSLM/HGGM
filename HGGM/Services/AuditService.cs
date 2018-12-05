3f04de60001e6bbc985024d22c4af21dc39d9f8e
Lasse Nielsen <lpnielsen@hotmail.com>
2018-12-05 13:30:50 +0100

    Details Overview & Minor change in auditservice

--- HGGM/Services/AuditService.cs
+++ HGGM/Services/AuditService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using HGGM.Models;
 using LiteDB;
 
@@ -26,7 +27,12 @@ namespace HGGM.Services
 
         public List<AuditEntryBase> GetAll()
         {
-            return db.Fetch<AuditEntryBase>();
+            return db.Fetch<AuditEntryBase>().OrderByDescending(a => a.Time).ToList();
+        }
+
+        public List<T> GetAll<T>() where T : AuditEntryBase
+        {
+            return db.Fetch<AuditEntryBase>().OfType<T>().OrderByDescending(a => a.Time).ToList();
         }
 
         public LiteQueryable<AuditEntryBase> Query()
